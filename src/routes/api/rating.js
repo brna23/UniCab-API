f3e07b05d846f3d9348ab06a62b107c19630526e
MattiaBernabe <mattybernabe@gmail.com>
2025-05-30 19:29:10 +0200

    delete e get recensioni

--- src/routes/api/rating.js
+++ src/routes/api/rating.js
@@ -7,8 +7,9 @@ const swagger = require('../../../swagger-definitions');
 const Prenotazione = require('../../models/booking');
 const Recensione = require('../../models/recensione');
 const User = require('../../models/user');
+const isAdmin = require('../../middleware/isAdmin');
 
-//get method for showing all user rating
+//metodo post per creare una recensione
 router.post('/:id', authMiddleware, async (req, res) => {
     try {
         const user = req.user;
@@ -43,6 +44,43 @@ router.post('/:id', authMiddleware, async (req, res) => {
 
     } catch (error) {
         console.error('Errore nella creazione della recensione:', error);
+        res.status(500).json({ error: 'Errore del server' });
+    }
+});
+
+//delete recensione solo se admin
+router.delete('/:id', authMiddleware, async (req, res)=> {
+    try{
+        const recensione = await Recensione.findById(req.params.id);
+        if (!recensione) {
+            return res.status(404).json({ error: 'Recensione non trovata' });
+        }
+        const canDelete = req.user.userId === recensione.originUser.toString();
+        if (!canDelete) {
+        return res.status(403).json({ error: 'Non autorizzato a cancellare questa recensione' });
+        }
+
+        if (canDelete || isUserAdmin(req.user)){
+            return res.status(200).json({ message: 'Recensione cancellata con successo' });
+        }else{
+            return res.status(403).json({ error: 'Non autorizzato a cancellare questa recensione' });
+        }
+    } catch(error) {
+        res.status(500).json({ error: 'Errore del server' });
+    }
+});
+
+//metodo get per ottenere tutte le recensioni riferite a un utente
+router.get('/user/:id', authMiddleware, async (req,res)=> {
+    try{
+        let query = { destinationUser: req.params.id}
+        const recensione = await Recensione.find(query).populate('originUser', 'username').populate('destinationUser', 'username');;
+        if (!recensione) {
+            return res.status(404).json({ error: 'Recensione non trovata' });
+        }
+        res.json(recensione)
+
+    }catch(error) {
         res.status(500).json({ error: 'Errore del server' });
     }
 });
